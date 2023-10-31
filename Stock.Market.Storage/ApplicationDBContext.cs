f2b9c0b62434c5a6798acf61eadd43e478f80dc4
luccanog <luccathomaz@gmail.com>
2023-10-31 17:34:51 -0300

    adding basic storage structures

---
+++ Stock.Market.Storage/ApplicationDBContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Stock.Market.Data.Entities;
+
+namespace Stock.Market.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions options) : base(options) { }
+
+        protected ApplicationDBContext() { }
+
+        public DbSet<Share> Shares { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                IConfigurationRoot configuration = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json")
+                    .Build();
+
+                var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+                optionsBuilder.UseSqlServer(connectionString);
+            }
+        }
+    }
+}
---
+++ Stock.Market.Storage/Entities/Share.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Stock.Market.Data.Entities
+{
+    public class Share
+    {
+        public Guid Id { get; set; } 
+
+        [Required]
+        public string CompanyName { get; set; }
+
+        [Required]
+        public string Symbol { get; set; }
+
+        [Required]
+        public decimal OriginalCost { get; set; }
+
+        public DateTime AcquisitionDate { get; set; }
+
+        public Share(string companyName, string symbol, decimal originalCost)
+        {
+            Id = Guid.NewGuid();
+            CompanyName = companyName;
+            Symbol = symbol;
+            OriginalCost = originalCost;
+        }
+    }
+}
