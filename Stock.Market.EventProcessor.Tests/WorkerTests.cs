114159ff756ef4d59a11b68a25c7412f8442c591
luccanog <luccathomaz@gmail.com>
2023-11-01 19:05:34 -0300

    wip - worker unit tests

--- Stock.Market.EventProcessor.Tests/WorkerTests.cs
+++ Stock.Market.EventProcessor.Tests/WorkerTests.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Stock.Market.Data;
+using Stock.Market.Data.Entities;
 using System.Text.RegularExpressions;
 
 namespace Stock.Market.EventProcessor.Tests
@@ -12,6 +13,7 @@ namespace Stock.Market.EventProcessor.Tests
         private readonly Worker _worker;
         private readonly Mock<ILogger<Worker>> _loggerMock;
         private readonly ServiceProvider _serviceProvider;
+        private readonly Fixture _fixture;
 
         public WorkerTests()
         {
@@ -33,16 +35,36 @@ namespace Stock.Market.EventProcessor.Tests
             _serviceProvider = serviceColletion.BuildServiceProvider();
             _loggerMock = new Mock<ILogger<Worker>>();
             _worker = new Worker(_loggerMock.Object, configuration, _serviceProvider);
+            _fixture = new Fixture();
         }
 
         [Fact]
-        public void Worker_ExecuteAsync_WithSellSharesEvent_AndAvailableSharesToBeSold_ShouldSucceed()
+        public async Task Worker_ExecuteAsync_WithSellSharesEvent_AndAvailableSharesToBeSold_ShouldSucceed()
         {
             // Arrange
+            var symbol = "AAPL";
+            var firstShares = _fixture.Build<Shares>().With(s => s.Symbol, symbol).With(s => s.Quantity, 5).Create();
+            var secondShares = _fixture.Build<Shares>().With(s => s.Symbol, symbol).With(s => s.Quantity, 5).Create();
+            InsertIntoDatabase(firstShares, secondShares);
+
+            /** @todo: mock kafka methods **/
+
+            //Act
+            await _worker.StartAsync(CancellationToken.None);
+            await Task.Delay(TimeSpan.FromSeconds(2));
+            await _worker.StopAsync(CancellationToken.None);
+
+            //Assert 
+            /** @todo: check database **/
+
+        }
+
+        private void InsertIntoDatabase(params Shares[] shares)
+        {
             using (var scope = _serviceProvider.CreateScope())
             {
                 var context = scope.ServiceProvider.GetRequiredService<ApplicationDBContext>();
-                context.Shares.Add(new Data.Entities.Shares());
+                context.Shares.AddRange(shares);
                 context.SaveChanges();
             }
         }
