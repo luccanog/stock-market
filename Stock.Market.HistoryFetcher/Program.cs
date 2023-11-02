1cfcad3bd36a5c79221a6b17ba9f0e71f1e6fc58
luccanog <luccathomaz@gmail.com>
2023-11-02 14:57:19 -0300

    adding service DI on history fetcher

--- Stock.Market.HistoryFetcher/Program.cs
+++ Stock.Market.HistoryFetcher/Program.cs
@@ -1,6 +1,8 @@
 using Stock.Market.Common.Services;
 using Stock.Market.Common.Services.Interfaces;
 using Stock.Market.Data;
+using Stock.Market.HistoryFetcher.Services;
+using Stock.Market.HistoryFetcher.Services.Interfaces;
 
 namespace Stock.Market.HistoryFetcher
 {
@@ -12,6 +14,7 @@ namespace Stock.Market.HistoryFetcher
                 .ConfigureServices(services =>
                 {
                     services.AddSingleton<INasdaqService, NasdaqService>();
+                    services.AddSingleton<IDataFetcherService, DataFetcherService>();
                     services.AddHostedService<Worker>();
                     services.AddDbContext<ApplicationDBContext>(ServiceLifetime.Singleton);
                 })
