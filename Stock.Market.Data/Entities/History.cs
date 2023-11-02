0156ea43c7285f9493e52b9428639386acd51b89
luccanog <luccathomaz@gmail.com>
2023-11-02 12:50:53 -0300

    adding history entity

---
+++ Stock.Market.Data/Entities/History.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Stock.Market.Data.Entities
+{
+    [Table(nameof(History))]
+    public class History
+    {
+        public Guid Id { get; set; }
+
+        [Required]
+        public string CompanyName { get; set; }
+
+        [Required]
+        public string Symbol { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+
+        [Required]
+        public DateTime InsertDate { get; set; }
+
+        public History(string companyName, string symbol, decimal price)
+        {
+            Id = Guid.NewGuid();
+            CompanyName = companyName;
+            Symbol = symbol;
+            Price = price;
+            InsertDate = DateTime.UtcNow;
+        }
+    }
+}
